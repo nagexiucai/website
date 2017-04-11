6f36847e2c9c26645dbf26c6ef420e187f2286c7
xiucai <xiucai@pahuashan.cn>
2017-04-11 18:16:48 +0800

    make up for starting vue.js in office

--- frontend/official.js
+++ frontend/official.js
@@ -1,9 +1,56 @@
 // https://github.com/nagexiucai/website
 
-window.onload(function(){
-var app = new Vue({
-  el: '#show',
-  data: {
-    message: 'Hello Vue!'
+var show = new Vue({
+  el:'#show',
+  data:{
+    message:'那个秀才'
   }
-})}())
+})
+
+var menu = new Vue({
+  el:'#menu',
+  data:{
+    message:'this is a tip',
+    items:[
+      {text:'流言', id:'note'},
+      {text:'愚乐', id:'game'},
+      {text:'首夜', id:'home'}
+    ],
+    show:true
+  },
+  methods:{
+    reverseMessage:function(){
+      show.message = show.message.split("").reverse().join("");
+    }
+  }
+})
+
+var logo = new Vue({
+  el:'#logo',
+  data:{
+    seen:true
+  }
+})
+
+var friend = new Vue({
+  el:'#friend',
+  data:{
+    things:'things'
+  }
+})
+
+Vue.component('list-item', {
+  props:['item'],
+  template:'<li v-bind:id=item.id>{{item.text}}</li>'
+})
+var the4classics = new Vue({
+  el:'#the4classics',
+  data:{
+    srcList:[
+      {text:'西游', id:'xiyou'},
+      {text:'三国', id:'sanguo'},
+      {text:'水浒', id:'shuihu'},
+      {text:'红楼', id:'honglou'}
+    ]
+  }
+})
