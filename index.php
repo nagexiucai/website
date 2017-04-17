368962d242a734ae634bb04451dcaeda805dc9f4
xiucai <xiucai@pahuashan.cn>
2017-04-17 17:34:00 +0800

    add logo-official.png

--- index.php
+++ index.php
@@ -7,11 +7,12 @@ class Index {
     public static $OFFICIAL = "nagexiucai.com";
     public static $TECH = "thisstack.com";
     public static $TOUR = "pahuashan.com";
-    public static $TEST = "website.local";
+    public static $Remote = "test.remote";
+    public static $Local = "test.local";
 
     function route() {
         $domain = implode(".", array_slice(explode(".", $_SERVER["HTTP_HOST"]), -2));
-        if ($domain == self::$TEST) {
+        if ($domain == self::$Remote || $domain == self::$Local) {
             $domain = $_GET["target"];
         }
         switch ($domain) {
