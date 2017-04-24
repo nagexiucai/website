ebcf8adae282f3cc917fc9ce24c1a5b1e659b9a8
xiucai <xiucai@pahuashan.cn>
2017-04-24 10:40:00 +0800

    try pythagoras tree in js

--- test.php
+++ test.php
@@ -1,11 +1,12 @@
 <?php
 // https://github.com/nagexiucai/website
 
-//include "./backend/blog/blog.php";
-//include "./backend/ebiz/ebiz.php";
-//include "./backend/tech/tech.php";
-//include "./backend/tour/tour.php";
-include "./backend/official/official.php";
+/*
+// include "./backend/blog/blog.php";
+// include "./backend/ebiz/ebiz.php";
+// include "./backend/tech/tech.php";
+// include "./backend/tour/tour.php";
+// include "./backend/official/official.php";
 exit();
 
 class Root {
@@ -48,3 +49,45 @@ function test_array_slice(){
     echo implode(".", array_slice(explode(".", $ss), -2)) . "<br/>";
     echo implode(".", array_slice(explode(".", $s), -2)) . "<br/>";
 }
+*/
+?>
+<html>
+<head>
+<title>Fractal Tree</title>
+</head>
+<body>
+<canvas id="canvas" width="600" height="500"></canvas>
+ 
+<script type="text/javascript">
+var elem = document.getElementById('canvas');
+var context = elem.getContext('2d');
+ 
+context.fillStyle = '#000';
+context.lineWidth = 1;
+ 
+var deg_to_rad = Math.PI / 180.0;
+var depth = 9;
+ 
+function drawLine(x1, y1, x2, y2, brightness){
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+}
+ 
+function drawTree(x1, y1, angle, depth){
+  if (depth !== 0){
+    var x2 = x1 + (Math.cos(angle * deg_to_rad) * depth * 10.0);
+    var y2 = y1 + (Math.sin(angle * deg_to_rad) * depth * 10.0);
+    drawLine(x1, y1, x2, y2, depth);
+    drawTree(x2, y2, angle - 20, depth - 1);
+    drawTree(x2, y2, angle + 20, depth - 1);
+  }
+}
+ 
+context.beginPath();
+drawTree(300, 500, -90, depth);
+context.closePath();
+context.stroke();
+</script>
+ 
+</body>
+</html>
