c297a5f32368f60fe31eb9b21c77641ba7866166
xiucai <xiucai@pahuashan.cn>
2017-05-08 16:10:01 +0800

    accomplish a simple index page of blog

--- frontend/blog.js
+++ frontend/blog.js
@@ -22,25 +22,103 @@ function indicator(which){
 
 // 在show区域展示图表
 var skills = {
-    "python":[{"field":"web", "color":"green", "weight":20}, {"field":"core", "color":"blue", "weight":30}, {"field":"cloud", "color":"yellow", "weight":20}],
-    "radio":[]
+    "python":[{"field":"wxwidgets","color":"violet","weight":0.6},
+              {"field":"tornado","color":"green","weight":0.7},
+              {"field":"interpreter","color":"navy","weight":0.6},
+              {"field":"sklearn","color":"blue","weight":0.5},
+              {"field":"openstack","color":"red","weight":0.8}],
+    "swift":[{"field":"iphone","color":"brown","weight":0.4},
+             {"field":"iwatch","color":"blue","weight":0.4}],
+    "go":[{"field":"docker","color":"red","weight":0.4},
+          {"field":"etcd","color":"green","weight":0.4}],
+    "php":[{"field":"laveral","color":"violet","weight":0.3},{"field":"ratchet ","color":"tan","weight":0.3}],
+    "javascript":[{"field":"node","color":"brown","weight":0.3},
+                  {"field":"vue","color":"green","weight":0.6},
+                  {"field":"d3","color":"blue","weight":0.5},
+                  {"field":"paper","color":"red","weight":0.5},
+                  {"field":"chart","color":"navy","weight":0.4},
+                  {"field":"egret","color":"navy","weight":0.7}],
+    "lua":[{"field":"elua","color":"violet","weight":0.4}],
+    "object-c":[{"field":"macos","color":"brown","weight":0.4}],
+    "erlang":[{"field":"leofs","color":"green","weight":0.4},
+              {"field":"otp","color":"blue","weight":0.4},
+              {"field":"elixir","color":"red","weight":0.3}],
+    "java":[{"field":"tomcat","color":"red","weight":0.5},
+            {"field":"struts","color":"blue","weight":0.6},
+            {"field":"android","color":"green","weight":0.7},
+            {"field":"quartz","color":"navy","weight":0.5},
+            {"field":"jfreechart","color":"brown","weight":0.4}],
+    "c++":[{"field":"ceph","color":"brown","weight":0.4},
+           {"field":"cocos2d","color":"blue","weight":0.4}],
+    "matlab":[{"field":"image","color":"brown","weight":0.4},
+              {"field":"communication","color":"blue","weight":0.3}],
+    "c":[{"field":"linux","color":"brown","weight":0.4},
+         {"field":"windows","color":"blue","weight":0.4},
+         {"field":"mcu","color":"red","weight":0.7},
+         {"field":"ucos","color":"brown","weight":0.4},
+         {"field":"redis","color":"navy","weight":0.4}],
+    "asm":[{"field":"x86","color":"blue","weight":0.4},
+           {"field":"arm","color":"red","weight":0.6}],
+    "hdl":[{"field":"vhdl","color":"brown","weight":0.4},
+           {"field":"verilog","color":"blue","weight":0.4}],
+    "chip":[{"field":"nano100","color":"brown","weight":0.4},
+            {"field":"lpc214x","color":"blue","weight":0.4},
+            {"field":"stm32f103x","color":"red","weight":0.4},
+            {"field":"stc89c52x","color":"green","weight":0.4},
+            {"field":"sim800c","color":"tan","weight":0.4},
+            {"field":"esp8266","color":"navy","weight":0.4},
+            {"field":"ns1315","color":"violet","weight":0.4}],
+    "pcblayout":[{"field":"altium","color":"brown","weight":0.7},
+                 {"field":"protel","color":"blue","weight":0.5},
+                 {"field":"cadence","color":"green","weight":0.4}],
+    "circuitdesign":[{"field":"multisim","color":"brown","weight":0.7},
+                     {"field":"proteus","color":"navy","weight":0.5}],
+    "algorithm":[{"field":"search","color":"red","weight":0.6},
+                 {"field":"filter","color":"navy","weight":0.5},
+                 {"field":"cnn","color":"brown","weight":0.7}],
+    "radio":[{"field":"radar","color":"brown","weight":0.7},
+             {"field":"antenna","color":"green","weight":0.3},
+             {"field":"hfss","color":"blue","weight":0.7}],
+    "biomedical":[{"field":"xray","color":"brown","weight":0.7},
+                  {"field":"ecg","color":"blue","weight":0.5}]
 };
-function powerspectrum(which){
+function powerSpectrum(which){
     $swf();
     abstract.style = "display:none;"
-    console.log(which.innerHTML);
     paper.style = "display:inline-block;";
     indicator(which);
     var name = which.attributes["src"].nodeValue;
     var data = skills[name];
     if (data){
-        // 饼图
-        var cxt=paper.getContext("2d");
-        cxt.fillStyle="#FF0000";
-        cxt.beginPath();
-        cxt.arc(240,200,30,0,Math.PI*2,true);
-        cxt.closePath();
-        cxt.fill();
+        console.log(which.innerHTML);
+        // 斑点
+        var stantardWeightRadius = paper.width * 0.125; // 标准比重斑点半径
+        var step = (Math.PI*2)/data.length;
+        var ctx = paper.getContext("2d");
+        var widthCenter = paper.width/2;
+        var heightCenter = paper.height/2;
+        ctx.clearRect(0,0,paper.width,paper.height);
+        for (var i=0; i<data.length; i++){
+            var weightRadiusInner = stantardWeightRadius * data[i].weight;
+            var weightRadiusOutter = weightRadiusInner + 2;
+            var flexFact = data.length/10 + 1;
+            var x = widthCenter + Math.cos(step*i)*stantardWeightRadius*flexFact;
+            var y = heightCenter + Math.sin(step*i)*stantardWeightRadius*flexFact;
+            ctx.fillStyle = "gray";
+            ctx.beginPath();
+            ctx.arc(x,y,weightRadiusOutter,0,Math.PI*2,true);
+            ctx.closePath();
+            ctx.fill();
+            ctx.fillStyle = data[i].color;
+            ctx.beginPath();
+            ctx.arc(x,y,weightRadiusInner,0,Math.PI*2,true);
+            ctx.closePath();
+            ctx.fill();
+            ctx.fillStyle = "white";
+            ctx.font = "20px Cursive";
+            ctx.textAlign = "center";
+            ctx.fillText(data[i].field,x,y+5); // TODO: 绘制文本垂直居中补丁
+        }
     }
 }
 
