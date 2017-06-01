79973b5e8245d242e896b39b906573ede9e6969d
xiucai <xiucai@pahuashan.cn>
2017-06-01 16:27:04 +0800

    add reward into topic

--- topic.php
+++ topic.php
@@ -11,7 +11,7 @@ class Topic extends Bra {
         {
             $this->svg = "/frontend/material-library/default.svg";
         }
-        $this->content = "<div class='topic'><object data='$this->svg' type='image/svg+xml'></object></div>";
+        $this->content = "<div class='topic'><object data='$this->svg' type='image/svg+xml'></object><p>长按二维码打赏</p><img src='/frontend/common/reward-wechat.png' alt='打赏二维码'></div>";
     }
 }
 
