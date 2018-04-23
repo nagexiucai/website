94858d351ae03f081cea7f4fa945b5075d42a179
bob-nx <me@nagexiucai.com>
2018-04-23 15:27:38 +0800

    update ping

--- ping.php
+++ ping.php
@@ -2,3 +2,14 @@
 // https://github.com/nagexiucai/website
 
 echo "nagexiucai.com";
+echo "<br/>";
+echo "POST";
+echo "<br/>";
+foreach($_POST as $k=>$v){
+    echo $k."===".$v;
+}
+echo "GET";
+echo "<br/>";
+foreach($_GET as $k=>$v){
+    echo $k."===".$v;
+}
